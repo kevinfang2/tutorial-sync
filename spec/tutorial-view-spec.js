9b5bd784ff1fab1332ae74218a2ecc41aea87007
kevinfang2 <kevkev0123@gmail.com>
2018-03-12 11:22:25 -0700

    initial commit

---
+++ spec/tutorial-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TutorialView from '../lib/tutorial-view';
+
+describe('TutorialView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
